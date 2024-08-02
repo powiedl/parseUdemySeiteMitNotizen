201a7e8d5787c4762e3fb8a925e54ab5097f4332
powiedl <159603185+powiedl@users.noreply.github.com>
2024-08-02 17:42:26 +0200

    viewing of notes is possible again

--- frontend-udemy-course-notes/src/features/course/CourseDetail.jsx
+++ frontend-udemy-course-notes/src/features/course/CourseDetail.jsx
@@ -1,11 +1,27 @@
 import { useParams } from "react-router-dom";
 import { useCurrentCourse } from "../../context/CurrentCourseContext";
+import SingleCard from "../notes/SingleCard";
+import Spinner from "../../ui/Spinner";
 
 export default function CourseDetail() {
   const { courseInfo, courseNotes } = useCurrentCourse();
 
-  const { courseId } = useParams();
-  console.log("CourseDetail,courseId=", courseId);
+  //const { courseId } = useParams();
+  console.log("CourseDetail,courseInfo=", courseInfo);
+  console.log("CourseDetail, # notes=", courseNotes.length);
+  console.dir(courseNotes[0]);
+  if (!courseNotes) return <Spinner />;
 
-  return <div>COURSE DETAIL (eigentlich die Notes)</div>;
+  return (
+    <>
+      {courseNotes.map((note) => (
+        <SingleCard key={note.key} note={note}>
+          <SingleCard.Section>{note.sectionHeading}</SingleCard.Section>
+          <SingleCard.Lesson>{note.lessonHeading}</SingleCard.Lesson>
+          <SingleCard.TimeStamp>{note.timestampStr}</SingleCard.TimeStamp>
+          <SingleCard.Note>{note.noteHTML}</SingleCard.Note>
+        </SingleCard>
+      ))}
+    </>
+  );
 }
---
+++ frontend-udemy-course-notes/src/features/notes/SingleCard.jsx
@@ -0,0 +1,168 @@
+//import React from 'react'
+import { createContext, useState, useContext, useEffect } from "react";
+import parse from "html-react-parser";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import styled from "styled-components";
+import ReactDOMServer from "react-dom/server";
+
+// #region Quill manipulationen
+const editorModules = {
+  toolbar: {
+    container: [
+      [{ header: [1, 2, 3, 4, false] }],
+      ["bold", "italic", "underline", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["code-block"],
+    ],
+    handlers: {},
+  },
+  clipboard: {
+    matchVisual: true,
+  },
+};
+// #endregion
+
+const StyledSingleCard = styled.div`
+  border: 2px solid black;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  border-radius: 0.3rem;
+  position: relative;
+`;
+
+const StyledNote = styled.div`
+  background-color: beige;
+  padding: 0.3rem 0.6rem;
+`;
+
+const StyledSection = styled.h4`
+  display: inline;
+  margin: 0.5rem 1.5rem;
+`;
+
+const StyledLession = styled.h5`
+  display: inline;
+  margin: 0.5rem 1.5rem;
+`;
+
+const StyledTimeStamp = styled.span`
+  margin: 0.5rem 1.5rem;
+`;
+
+const StyledButton = styled.button`
+  position: absolute;
+  top: 0%;
+  right: 0%;
+  transition: all 0.25s;
+  opacity: 100%;
+  background-color: ${(props) =>
+    props.edit === "true" ? "blue" : "lightgray"};
+  &:hover {
+    transform: scale(1.2);
+    transition: all 0.25s;
+    opacity: 80%;
+  }
+`;
+
+const SingleCardContext = createContext();
+export default function SingleCard({ children, note }) {
+  const [edit, setEdit] = useState(false);
+  const [convertedText, setConvertedText] = useState(note.noteHTML);
+  const [savedText, setSavedText] = useState("");
+  const changed = note.noteHTML !== convertedText;
+  function handleEditClick() {
+    //console.log("handleClick edit=", edit);
+    setEdit((e) => !e);
+    setConvertedText((t) => correctConvertedText(t));
+  }
+  function correctConvertedText(t) {
+    // remove multiple <p><br></p> in a row
+    const pattern = "<p><br></p>";
+    const newText = t
+      .split(pattern)
+      .filter((e) => e)
+      .join(pattern);
+    // split string at each occurence of pattern, ...
+    // ... if multiple patterns occur in sccession then the splitted element is an empty string
+    // ... so filter out empty elements - an empty string is falsy, so filter filters it out
+    // ... join the elements of the resulting array with the pattern - so the pattern is guaranteed to be only one at a time
+    return newText;
+  }
+  return (
+    <StyledSingleCard>
+      <SingleCardContext.Provider
+        value={{
+          edit,
+          convertedText,
+          setConvertedText,
+          changed,
+          note,
+        }}
+      >
+        {children}
+        <EditButton onClick={handleEditClick}>🖊</EditButton>
+      </SingleCardContext.Provider>
+    </StyledSingleCard>
+  );
+}
+
+function Section({ children }) {
+  return <StyledSection>{children}</StyledSection>;
+}
+
+function Lesson({ children }) {
+  return <StyledLession>{children}</StyledLession>;
+}
+
+function TimeStamp({ children }) {
+  return <StyledTimeStamp>{children}</StyledTimeStamp>;
+}
+
+function Note({ children }) {
+  const { edit, convertedText, setConvertedText, changed, note } =
+    useContext(SingleCardContext);
+  //const [editContent, setEditContent] = useState();
+  // useEffect(() => {
+  //   //const str = ReactDOMServer.renderToString({ children });
+  //   setConvertedText(note.noteHTML);
+  // }, [setConvertedText, note]);
+  //console.log("convertedText=", convertedText);
+  return (
+    <StyledNote>
+      {changed && "CHANGED! "}
+      {edit ? (
+        <>
+          <ReactQuill
+            theme='snow'
+            value={convertedText}
+            onChange={setConvertedText}
+            // style={{ minHeight: "300px" }}
+            modules={editorModules}
+          />
+        </>
+      ) : (
+        <>{parse(convertedText)}</>
+      )}
+    </StyledNote>
+  );
+}
+
+function EditButton({ onClick }) {
+  const { edit } = useContext(SingleCardContext);
+  return (
+    <StyledButton onClick={onClick} edit={edit.toString()}>
+      🖊
+    </StyledButton>
+  );
+}
+
+SingleCard.Section = Section;
+SingleCard.Lesson = Lesson;
+SingleCard.TimeStamp = TimeStamp;
+SingleCard.Note = Note;
